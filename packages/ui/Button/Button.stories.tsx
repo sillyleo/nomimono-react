2924d05910d6c8d439aa200dd67be55f3d417810
Leo Lin <sayaww@gmail.com>
2023-03-17 17:22:35 +0800

    update

--- packages/ui/Button/Button.stories.tsx
+++ packages/ui/Button/Button.stories.tsx
@@ -1,13 +1,14 @@
 // Button.stories.ts|tsx
 
 import type { Meta, StoryObj } from "@storybook/react";
-
-// import directly from the component file
+import { withDesign } from "storybook-addon-designs";
 import { Button } from "./";
 
 const meta: Meta<typeof Button> = {
 	title: "Button",
 	component: Button,
+	decorators: [withDesign],
+
 	tags: ["autodocs"],
 	args: {
 		children: "Button",
@@ -19,4 +20,10 @@ type Story = StoryObj<typeof Button>;
 
 export const Primary: Story = {
 	args: {},
+	parameters: {
+		design: {
+			type: "figma",
+			url: "https://www.figma.com/file/QSPc25lxtbRsoqZE6CjcOm/Design-System-old?node-id=1520-4007&t=fJFbo5mEg4eXoDOc-4",
+		},
+	},
 };
