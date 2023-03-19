f046a293f5138d70e2a0e70e0fc2762e1e200541
Leo Lin <sayaww@gmail.com>
2023-03-19 19:48:11 +0800

    update embed

--- apps/docs/misc/Story.tsx
+++ apps/docs/misc/Story.tsx
@@ -1,10 +1,10 @@
 import { Display } from "nomimono";
 
-export const Story = ({ id }: { id: string }) => {
+export const Story = ({ url }: { url: string }) => {
 	return (
 		<Display css={{ p: 0 }}>
 			<iframe
-				src={`https://nomi-workshop.vercel.app/iframe.html?args=&id=${id}--primary&viewMode=story&shortcuts=true&singleStory=true`}
+				src={`${url}&shortcuts=true&singleStory=true`}
 				width="100%"
 				height="130"
 			></iframe>
