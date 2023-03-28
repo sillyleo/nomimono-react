2977c192eb7c0173d8695b6f1b3e6153344dccbc
Leo Lin <sayaww@gmail.com>
2023-03-28 16:57:57 +0800

    Update Dialog.stories.tsx

--- packages/ui/stories/Dialog.stories.tsx
+++ packages/ui/stories/Dialog.stories.tsx
@@ -6,42 +6,42 @@ import { Button } from "../components/Button";
 import { Dialog } from "../components/Dialog";
 
 const DialogDefault = () => {
-	return (
-		<Dialog.Root>
-			<Dialog.Trigger asChild>
-				<Button>Open Dialog with Close + Scroll</Button>
-			</Dialog.Trigger>
-			<Dialog.Portal>
-				<Dialog.Overlay>
-					<Dialog.ContentWithClose>
-						{/* repeat paragraph 10 times */}
-						{[...Array(20)].map((_, i) => (
-							<Text key={i}>
-								Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
-								massa egestas mollis varius; dignissim elementum. Mollis
-								tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
-								Hendrerit parturient habitant pharetra rutrum gravida porttitor
-								eros feugiat. Mollis elit sodales taciti duis praesent id.
-								Consequat urna vitae morbi nunc congue.
-							</Text>
-						))}
-						<Dialog.Close asChild>
-							<Button>Close Button in content</Button>
-						</Dialog.Close>
-					</Dialog.ContentWithClose>
-				</Dialog.Overlay>
-			</Dialog.Portal>
-		</Dialog.Root>
-	);
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <p>Open Dialog with Close + Scroll</p>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay>
+          <Dialog.ContentWithClose>
+            {/* repeat paragraph 10 times */}
+            {[...Array(20)].map((_, i) => (
+              <Text key={i}>
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
+                massa egestas mollis varius; dignissim elementum. Mollis
+                tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
+                Hendrerit parturient habitant pharetra rutrum gravida porttitor
+                eros feugiat. Mollis elit sodales taciti duis praesent id.
+                Consequat urna vitae morbi nunc congue.
+              </Text>
+            ))}
+            <Dialog.Close asChild>
+              <Button>Close Button in content</Button>
+            </Dialog.Close>
+          </Dialog.ContentWithClose>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
 };
 const meta: Meta<typeof Dialog> = {
-	title: "Atomic/Dialog",
-	component: DialogDefault,
+  title: "Atomic/Dialog",
+  component: DialogDefault
 };
 
 export default meta;
 type Story = StoryObj<typeof Dialog>;
 
 export const Default: Story = {
-	args: {},
+  args: {}
 };
