a5a623c531be424dbcaa8e434091a78223bcb43e
Leo Lin <sayaww@gmail.com>
2023-04-09 23:19:47 +0800

    update sb 7

--- packages/nomi-mono/.storybook/main.ts
+++ packages/nomi-mono/.storybook/main.ts
@@ -1,21 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-
 const config: StorybookConfig = {
-  stories: [
-    "../*.mdx",
-    "../src/stories/*.mdx",
-    "../src/stories/*.stories.@(js|jsx|ts|tsx)",
-    "../src/components/**/*.stories.@(js|jsx|ts|tsx)",
-    "../src/components/**/*.mdx"
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-docs",
-    "@storybook/addon-styling"
-
-  ],
+  stories: ["../*.mdx", "../src/stories/*.mdx", "../src/stories/*.stories.@(js|jsx|ts|tsx)", "../src/components/**/*.stories.@(js|jsx|ts|tsx)", "../src/components/**/*.mdx"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-docs", "@storybook/addon-styling", "@storybook/addon-mdx-gfm"],
   framework: {
     name: "@storybook/react-vite",
     options: {}
